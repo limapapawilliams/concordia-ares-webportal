820ceece9f243c730287e8825773443e685d5f70
Duke Whisky <55602167+limapapawilliams@users.noreply.github.com>
2023-06-25 06:25:18 +0100

    Add files via upload

--- app/custom-routes.js
+++ app/custom-routes.js
@@ -2,5 +2,7 @@ export default function setupCustomRoutes(router) {
   // Define your own custom routes here, just as you would in router.js but using 'router' instead of 'this'.
   // For example:
   // router.route('yourroute');
-  
+ router.route('renown-management');
+ router.route('renown-management-org', { path: '/renown-management/:org' });
+ router.route('renown-top');
 }
